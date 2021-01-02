43f5e8cac9cda11c145d6fdeb64758d3bd25cf12
Takahiro Abe <hirotaka081630@gmail.com>
2021-01-03 05:28:06 +0900

    p45 クッキー

--- 5.go
+++ 5.go
@@ -17,9 +17,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
+func handler45(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	if _, ok := r.Header["Cookie"]; ok {
+		// クッキーがあるということは一度来たことがある人
+		fmt.Fprintf(w, "<html><body>2回目以降</body></html>\n")
+	} else {
+		fmt.Fprintf(w, "<html><body>初訪問</body></html>\n")
+	}
+}
+
 func main() {
 	var httpServer http.Server
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler45)
 	log.Println("start http listennig :18888")
 	httpServer.Addr = ":18888"
 	log.Println(httpServer.ListenAndServe())
