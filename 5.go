a8fdc556d9295e087f09fa8113fea3828c675762
Takahiro Abe <hirotaka081630@gmail.com>
2021-01-03 06:17:48 +0900

    p54

--- 5.go
+++ 5.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/k0kubun/pp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -27,9 +30,27 @@ func handler45(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlerDigest(w http.ResponseWriter, r *http.Request) {
+	pp.Printf("URL: %s\n", r.URL.String())
+	pp.Printf("Query: %v\n", r.URL.Query())
+	pp.Printf("Proto: %s\n", r.Proto)
+	pp.Printf("Method: %v\n", r.Method)
+	pp.Printf("Header: %v\n", r.Header)
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("--body--\n%s\n", string(body))
+	if _, ok := r.Header["Authorization"]; !ok {
+		w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone", nonce="TgLc25U2BQA=f510a2780473e18e6587be702c2e67fe2b04afd", algorithm=MD5, qop="auth"`)
+		w.WriteHeader(http.StatusUnauthorized)
+	} else {
+		fmt.Fprintf(w, "<html><body>secret page</body></html>\n")
+	}
+}
+
 func main() {
 	var httpServer http.Server
 	http.HandleFunc("/", handler45)
+	http.HandleFunc("/digest", handlerDigest)
 	log.Println("start http listennig :18888")
 	httpServer.Addr = ":18888"
 	log.Println(httpServer.ListenAndServe())
